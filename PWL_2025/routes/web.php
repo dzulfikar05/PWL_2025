a8368a2c5b5b29ed3245be4a6b58255f3ee3f942
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-02-18 19:38:28 +0700

    method berparam

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -128,7 +128,7 @@ use Illuminate\Support\Facades\Route;
 // Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
 
 
-Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/hello', [WelcomeController::class, 'hello']);
 // Route::get('/', [PageController::class, 'index']);
 // Route::get('/about', [PageController::class, 'about']);
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
@@ -141,8 +141,19 @@ Route::get('/articles/{id}', ArticleController::class);
 
 // Route::resource('photos', PhotoController::class);
 
-Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
 ]);
 
-Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
 ]);
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Andi']);
+// });
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
