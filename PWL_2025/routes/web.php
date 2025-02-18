b173ca03e0bd2b1bc1b28729326712f59961981a
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-02-18 19:40:05 +0700

    save route

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -156,4 +156,5 @@ Route::resource('photos', PhotoController::class)->except([
 // Route::get('/greeting', function () {
 //     return view('blog.hello', ['name' => 'Andi']);
 // });
+
 Route::get('/greeting', [WelcomeController::class, 'greeting']);
