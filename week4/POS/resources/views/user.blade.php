e998fdceb88c012820a682b50249231e4714bb07
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-03-04 21:39:37 +0700

    Praktikum-2.3 : week 4

--- week4/POS/resources/views/user.blade.php
+++ week4/POS/resources/views/user.blade.php
@@ -9,6 +9,14 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+    </table>
+    {{-- <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
@@ -20,7 +28,7 @@
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr>
-    </table>
+    </table> --}}
 </body>
 
 </html>
