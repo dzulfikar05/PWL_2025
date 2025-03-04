7e29e19f29436a931b0d3691d99dd0a6da0bd832
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-03-04 22:51:52 +0700

    Praktikum-2.6 : week 4

--- week4/POS/resources/views/user.blade.php
+++ week4/POS/resources/views/user.blade.php
@@ -7,27 +7,31 @@
 
 <body>
     <h1>Data User</h1>
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr>
-    </table> --}}
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>
+    <a href="/user/tambah">+ Tambah User</a>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                    <td>
+                        <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> |
+                        <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 </body>
 
