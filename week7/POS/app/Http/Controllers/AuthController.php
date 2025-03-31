7c29e229eb05d4dca96d393dc4d9e4ae91c291cc
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-03-31 18:51:34 +0700

    week 7 : tugas

--- week7/POS/app/Http/Controllers/AuthController.php
+++ week7/POS/app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -42,5 +45,51 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function postRegister(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'username' => 'required|string|max:255|unique:m_user',
+                'password' => 'required|string|min:6',
+            ]);
+
+            // Cari ID level CUS
+            $cusLevel = LevelModel::where('level_kode', 'CUS')->first();
+            if (!$cusLevel) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Role CUS tidak ditemukan.',
+                ]);
+            }
+
+            $user = UserModel::create([
+                'level_id' => $cusLevel->level_id,
+                'username' => $request->username,
+                'nama' => $request->name,
+                'password' => $request->password,
+            ]);
+
+            if ($user) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Registrasi Berhasil',
+                    'redirect' => url('login'),
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Registrasi Gagal',
+                ]);
+            }
+        }
+        return redirect('register');
     }
 }
--- week7/POS/resources/views/auth/register.blade.php
+++ week7/POS/resources/views/auth/register.blade.php
@@ -4,26 +4,38 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Pengguna</title>
+    <title>Registrasi Pengguna</title>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
+<body class="hold-transition register-page">
+    <div class="register-box">
         <div class="card card-outline card-primary">
-            <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a></div>
+            <div class="card-header text-center">
+                <a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a>
+            </div>
             <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-                <form action="{{ url('login') }}" method="POST" id="form-login">
+                <p class="login-box-msg">Register a new membership</p>
+
+                <form action="{{ url('register') }}" method="POST" id="form-register">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="text" id="username" name="username" class="form-control"
-                            placeholder="Username">
+                        <input type="text" id="name" name="name" class="form-control" placeholder="Full name">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                        <small id="error-name" class="error-text text-danger"></small>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" id="username" name="username" class="form-control" placeholder="Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -42,22 +54,16 @@
                         <small id="error-password" class="error-text text-danger"></small>
                     </div>
                     <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember"><label for="remember">Remember Me</label>
-                            </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
                         </div>
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        </div>
+                    </div>
                 </form>
-                <p class="mt-3 mb-1">
-                    <a href="{{ url('register') }}">Register a new membership</a>
-                </p>
-            </div>
+
+                <a href="{{ url('login') }}" class="text-center">I already have a membership</a>
             </div>
         </div>
+    </div>
     <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
@@ -72,8 +78,13 @@
         });
 
         $(document).ready(function() {
-            $("#form-login").validate({
+            $("#form-register").validate({
                 rules: {
+                    name: {
+                        required: true,
+                        minlength: 3,
+                        maxlength: 50
+                    },
                     username: {
                         required: true,
                         minlength: 4,
@@ -86,6 +97,11 @@
                     }
                 },
                 messages: {
+                    name: {
+                        required: "Nama lengkap wajib diisi",
+                        minlength: "Nama minimal 3 karakter",
+                        maxlength: "Nama maksimal 50 karakter"
+                    },
                     username: {
                         required: "Username wajib diisi",
                         minlength: "Username minimal 4 karakter",
@@ -97,14 +113,14 @@
                         maxlength: "Password maksimal 20 karakter"
                     }
                 },
-                submitHandler: function(form) { // ketika valid, maka bagian yg akan dijalankan
+                submitHandler: function(form) {
                     $.ajax({
                         url: form.action,
                         type: form.method,
                         data: $(form).serialize(),
                         dataType: 'json',
                         success: function(response) {
-                            if (response.status) { // jika sukses
+                            if (response.status) {
                                 Swal.fire({
                                     icon: 'success',
                                     title: 'Berhasil',
@@ -112,7 +128,7 @@
                                 }).then(function() {
                                     window.location = response.redirect;
                                 });
-                            } else { // jika error
+                            } else {
                                 $('.error-text').text('');
                                 if (response.msgField) {
                                     $.each(response.msgField, function(prefix, val) {
