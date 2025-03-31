b6864154e4e501dfa9e4403952d2c7c18ab262be
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-03-31 18:34:15 +0700

    week 7 : praktikum 3

--- week7/POS/app/Http/Middleware/AuthorizeUser.php
+++ week7/POS/app/Http/Middleware/AuthorizeUser.php
@@ -13,16 +13,13 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user(); // Ambil data user yang sedang login
-
-        // Periksa apakah user memiliki role yang sesuai
-        if ($user && $user->hasRole($role)) {
-            return $next($request);
+        $user_role = $request->user()->getRole(); // ambil data level_kode dari user yg login
+        if (in_array($user_role, $roles)) { // cek apakah level_kode user ada di dalam array roles
+            return $next($request); // jika ada, maka lanjutkan request
         }
-
-        // Jika tidak memiliki akses, tampilkan error 403
+        // jika tidak punya role, maka tampilkan error 403
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
 }
