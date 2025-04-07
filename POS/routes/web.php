a459024ae12f5d9c565d0f20c6162fdfd0aa78b7
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-04-07 19:17:58 +0700

    init auth

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('register', [AuthController::class, 'postRegister']);
+
